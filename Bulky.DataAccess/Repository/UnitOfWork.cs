e19d2b38f19398a03933ed2f14118a0884e4547d
saitienisa <saitienisa@outlook.com>
2023-10-03 17:19:09 +0200

    #commit all changes #All files changed

---
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using BulkyDataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public ICompanyRepository Company { get; private set; }
+        public IProductRepository Product { get; private set; }
+        public IShoppingCartRepository ShoppingCart {  get; private set; }   
+        public IApplicationUserRepository ApplicationUser { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            Company = new CompanyRepository(_db);
+            Product = new ProductRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
