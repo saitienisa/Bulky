9784108e7e9b4cd9d6f4b247a8f8ba62c5ddec44
saitienisa <saitienisa@outlook.com>
2023-10-03 17:19:09 +0200

    #All files changed

---
+++ Bulky.DataAccess/Migrations/20231002184913_AddCompanyRecords.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetName" },
+                values: new object[,]
+                {
+                    { 1, "Tech City", "Tech Solution", "1", "1", "IL", "123 Tech St" },
+                    { 2, "Vid City", "Vivid Books", "2", "2", "IL", "99 Vid Sr" },
+                    { 3, "Main City", "Readers Club", "3", "3", "IL", "123 Main St" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using BulkyDataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+            {
+                _db = db;
+            }
+    }
+}
